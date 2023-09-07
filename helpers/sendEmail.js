2d9d93fcabf5a6896e9148005dddf78bb5bd74a3
Uliana <ulianaastra1@gmail.com>
2023-09-08 01:35:05 +0300

    update sendEmail

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,17 +1,25 @@
-const mgMail = require('mailgun.js');
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
 
-const {MAILGUN_API_KEY} = process.env;
+const { MAILGUN_API_KEY } = process.env;
 
-mgMail.setApiKey(MAILGUN_API_KEY);
+const mailgun = new Mailgun(formData);
+require("dotenv").config();
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "ulianaastra1@gmail.com" };
-  try {
-    await mgMail.send(email);
-    return true;
-  } catch (err) {
-    throw new Error(err);
-  }
+  const mg = mailgun.client({
+    username: "ulianaastra1@gmail.com",
+    key: MAILGUN_API_KEY,
+  });
+  mg.messages
+    .create("sandbox66b2be7e6692473f998f75d202d8785d.mailgun.org", {
+      from: "Mailgun Sandbox <ulianaastra1@gmail.com>",
+      to: [data.to],
+      subject: "Verify your email",
+      text: "Verify your email",
+      html: data.html,
+    })
+    .then((msg) => console.log(msg))
+    .catch((err) => console.log(err));
 };
-
 module.exports = sendEmail;
